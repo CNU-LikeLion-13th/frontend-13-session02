578d2150d5253744dbb8c22e732eab75b85f3c3f
als8921 <als8921@naver.com>
2025-04-01 15:14:19 +0900

    mission03 update

--- day02/mission03.js
+++ day02/mission03.js
@@ -4,9 +4,22 @@
 이름을 인자로 받아서 다른 함수(콜백 함수)를 통해 출력 형식을 바꿔보세요.
 
 콜백 함수와 대문자로 바꾸는 메서드를 사용해서 만들어주세요!
+
+구현해야 하는 메서드는 다음과 같습니다.
+
+1. 이름을 대문자로 바꾸어 출력하는 메서드
+2. "안녕하세요, (입력한 글자)님" 형태로 바꾸어 출력하는 메서드
+
+- 모든 문제는 console.log 를 통해 출력해 확인할 수 있습니다.
+
+console.log문은 다음과 같습니다!
+
+console.log(formatName("본인 이름(소문자로)", upperCase));
+
+console.log(formatName("본인 이름(소문자로)", greeting));
 */
 
-const changeFormat = (e, callback) => callback(e);
+const formatName = (e, callback) => "안녕하세요, " + callback(e) + "님";
 const upperCase = (e) => e.toUpperCase();
 
-console.log(changeFormat("John", upperCase));
+console.log(formatName("mincheol", upperCase));
