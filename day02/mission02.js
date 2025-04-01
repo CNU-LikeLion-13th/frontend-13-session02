a45ee6a0741a9e912f24cc9e3239424fca1b00fe
JuHeyong <wkdwngud963@gmail.com>
2025-04-01 23:17:00 +0900

    추가 사항

--- day02/mission02.js
+++ day02/mission02.js
@@ -1,9 +1,9 @@
 
 function mapWithCallback(arr, callback) {
-    return arr.map(callback);
+    return callback(arr);
 }
 
-const double = a => a * 2;
+const double = arr => arr.map(a => a * 2);
 const addTen = a => a + 10;
 
 console.log(mapWithCallback([1, 2, 3], double));
--- day02/mission03.js
+++ day02/mission03.js
@@ -1,7 +1,12 @@
-function upperWithCallback(name, callback) {
+function formatName(name, callback) {
     return callback(name);
 }
 
-const upper = name => name.toUpperCase();
+const upperCase = name => name.toUpperCase();
+function greeting(name) {
+    return `안녕하세요, ${name}님`;
+}
 
-console.log(upperWithCallback("asdfzxcv", upper));
+console.log(formatName("jang ju hyeong", upperCase));
+console.log(formatName("jang ju hyeong", greeting));
+
